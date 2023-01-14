4e7d0c2d9a347a5a5865ae6c11e6c3f9f40518c9
Alan Gonzalez <alanfga1@gmail.com>
2023-01-14 02:38:37 -0700

    añadir routing de las url

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', '');
+$db = mysqli_connect('localhost', 'root', 'admin', 'uptask_mvc');
 
 
 if (!$db) {
--- public/index.php
+++ public/index.php
@@ -2,9 +2,32 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+//login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+
+//logout
+$router->get('/logout', [LoginController::class, 'logout']);
+
+//crear cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
+
+//formulario de olvide mi password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+
+//colocar nuevo password
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+//confirmacion de cuenta
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
 
 
 
